1673e9f8b2e0a010ea58f9bdeaa8601c00807cb7
Paulo Cezar Motta <paulocezarmotta@gmail.com>
2022-05-06 14:25:35 -0300

    Chapter VI - Deploy - 5.5 - Configurando domínio e SSL

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -10,10 +10,12 @@ import swaggerFile from '../../../swagger.json'
 import { router } from './routes'
 import { AppError } from '@shared/errors/AppError'
 import upload from '@config/upload'
+import cors from 'cors'
 
 createConnection()
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use(router)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
